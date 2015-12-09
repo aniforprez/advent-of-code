0f871a44849c91db33fe14e0f1efd50eb002e1ef
Anirudh Sylendranath <ani.22691@gmail.com>
2015-12-10 00:20:47 +0530

    second day of advent: done

--- index.js
+++ index.js
@@ -18,3 +18,42 @@ day1Data.split('').forEach(function(instruction, index) {
 });
 
 console.log(santasFloor, santaEntersBasement);
+
+var day2Data = require('./advent-day2-data').data;
+// day2Data = '1x1x10';
+
+var packingNeeded = 0;
+var ribbonNeeded = 0;
+day2Data.split(' ').forEach(function(dimensions, index) {
+	var sides = dimensions.split('x');
+
+	sides.forEach(function(side, index) {
+		sides[index] = parseInt(side);
+	});
+	smallestSides = [];
+	sides = sides.sort(function(a, b) {
+		if(a < b) {
+			return -1;
+		}
+		if(a > b) {
+			return 1;
+		}
+		if(a == b) {
+			return 0;
+		}
+	});
+	pseudoSides = sides;
+	smallestSides = sides.slice(0, 2);
+
+	var face1 = sides[0] * sides[1];
+	var face2 = sides[2] * sides[1];
+	var face3 = sides[0] * sides[2];
+	var smallestFace = face1;
+
+	packingNeeded += 2 * (face1 + face2 + face3);
+	packingNeeded += smallestFace;
+
+	ribbonNeeded += (smallestSides[0] * 2) + (smallestSides[1] * 2) + (sides[0] * sides[1] * sides[2]);
+});
+
+console.log(packingNeeded, ribbonNeeded);
